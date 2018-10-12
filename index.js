0983139aff974d12074e6407d624d78cb7c4108f
Samuel Moritz <samuel.moritz@gmail.com>
2018-10-12 14:54:12 +0200

    WASD Movement

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  io.emit('user_action', {id: "Samuel", x: 50, y: 50});
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
+  });
+  socket.on('user_action', function(data) {
+      io.emit('user_action', data);
   });
 });
 
